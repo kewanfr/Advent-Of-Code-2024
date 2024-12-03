86105a85614f7ec8e8fa62febf891bc95f351737
Kéwan B <mail@kewan.fr>
2024-12-03 17:08:47 +0100

    test V2

--- template_test.go
+++ template_test.go
@@ -1,27 +1,31 @@
 package template
 
 import (
+	"fmt"
 	"testing"
 )
 
 func TestPartOne(t *testing.T){
 	t.Run("Part", func(t *testing.T) {
-		expected := 0
+		// expected := 0
 		actual := partone("input.txt")
 		
-		if expected != actual{
-			t.Fail()
-		}
+		fmt.Println("Part One: ", actual)
+		// if expected != actual{
+		// 	t.Fail()
+		// }
 	})
 }
 
 func TestPartTwo(t *testing.T){
 	t.Run("Part", func(t *testing.T) {
-		expected := 0
+		// expected := 0
 		actual := parttwo("input.txt")
+
+		fmt.Println("Part Two: ", actual)
 		
-		if expected != actual{
-			t.Fail()
-		}
+		// if expected != actual{
+		// 	t.Fail()
+		// }
 	})
 }
